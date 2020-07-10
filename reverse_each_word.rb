4b06f3de7d7eff5dbcc53d7c283063c0dd502add
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-07-10 14:40:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,6 @@
 def reverse_each_word(sentence)
   sentence.collect.split(" ") do |word| = reversed_sentence
     word.reverse
-    reversed_sentence.join("")
   end
+  reversed_sentence.join("")
 end
